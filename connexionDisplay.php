bbfc930ee1d2115040c5b257e2bf2f84b5a2fd04
Systolicious <123855559+Systolicious@users.noreply.github.com>
2023-03-16 09:50:54 +0100

    sqlpush

--- connexionDisplay.php
+++ connexionDisplay.php
@@ -21,3 +21,35 @@ $monVisiteur = new Visiteur(
 <li>Email :
     <?php echo $monVisiteur->getEmail(); ?>
 </li>
+
+<!-- read the data from the database -->
+<?php
+$dbh = new PDO("mysql:dbname=BuyECar;port=8889", "root", "root");
+$result = $dbh->prepare('SELECT * FROM Visiteurs WHERE nom=:nom AND prenom=:prenom AND email=:email AND motDePasse=:motDePasse');
+
+$result->bindValue(':nom', $monVisiteur->getNom(), PDO::PARAM_STR);
+$result->bindValue(':prenom', $monVisiteur->getPrenom(), PDO::PARAM_STR);
+$result->bindValue(':email', $monVisiteur->getEmail(), PDO::PARAM_STR);
+$result->bindValue(':motDePasse', $monVisiteur->getMotDePasse(), PDO::PARAM_STR);
+
+$result->execute();
+
+$results = $result->fetchAll(); ?>
+
+<!-- write the data into the database -->
+
+<?php
+$dbh = new PDO("mysql:dbname=BuyECar;port=8889", "root", "root");
+
+// Prepare the SQL query
+$sql = "INSERT INTO Visiteurs (nom, prenom, email, motDePasse) VALUES (:nom, :prenom, :email, :motDePasse)";
+$stmt = $dbh->prepare($sql);
+
+// Bind the values to the parameters in the query
+$stmt->bindValue(':nom', $monVisiteur->getNom());
+$stmt->bindValue(':prenom', $monVisiteur->getPrenom());
+$stmt->bindValue(':email', $monVisiteur->getEmail());
+$stmt->bindValue(':motDePasse', $monVisiteur->getMotDePasse());
+
+// Execute the query
+$stmt->execute(); ?>
