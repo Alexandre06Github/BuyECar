4e8ca34b80d4dcfcddf0a8bc542be310794dfb9b
Aamndine <amandine.l@live.fr>
2023-03-16 15:20:36 +0100

    cssnavbar

--- index.php
+++ index.php
@@ -5,14 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>eCar enchère</title>
 
 </head>
 
 <body>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Titre de la page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<img src="sanstitre2.jpg" alt="Description de l'image">
+	</header>
+	<nav>
+		<div class="logo">
+			<img src="logo.png" alt="Description du logo">
+		</div>
+		<ul>
+  <li><a class="btn" href="#">Accueil</a></li>
+  <li><a class="btn" href="#">Déposer une annonce</a></li>
+  <li><a class="btn" href="#">Inscription</a></li>
+  <li><a class="btn" href="#">Connexion</a></li>
+</ul>
+<div class="wrapper">
+  <div class="nav"></div>
+  <div class="car"></div>
+</div>
+	</nav><br><br>
     <h1>Vente aux enchères</h1>
-    <h2>Renseignez votre véhicule</h2>
+    <h2>Renseignez votre véhicule</h2><br><br>
 
     <form action="voitureAnnonce.php" method="POST">
 
@@ -66,7 +92,7 @@
 
 
 
-
+    script.js"></script>
 </body>
 
 </html>
