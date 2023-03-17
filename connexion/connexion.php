cee80fc107c68bb9b0ec497631fa3dcd6f55d674
Alexandre06Github <124672024+Alexandre06Github@users.noreply.github.com>
2023-03-17 14:23:14 +0100

    ROOT_PATH

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -1,5 +1,27 @@
-<h2>Se connecter</h2>
+<?php define('ROOT_PATH', '../'); ?>
 
-<input type="text" placeholder="email">
-<input type="password" placeholder="mot de passe">
-<button>Se connecter</button>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se connecter</title>
+    <link rel="stylesheet" href="../style.css">
+
+</head>
+
+<body>
+
+    <?php include('../navbar.php'); ?>
+
+    <h2>Se connecter</h2>
+
+    <input type="text" placeholder="email">
+    <input type="password" placeholder="mot de passe">
+    <button>Se connecter</button>
+
+</body>
+
+</html>
--- inscription/inscription.php
+++ inscription/inscription.php
@@ -1,3 +1,5 @@
+<?php define('ROOT_PATH', '../'); ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -5,13 +7,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>eCar enchères</title>
+    <title>S'inscrire</title>
+    <link rel="stylesheet" href="../style.css">
 
 </head>
 
 <body>
 
+    <?php include('../navbar.php'); ?>
+
     <h1>Création de compte</h1>
+    <br>
     <h2>Renseignez votre profil</h2>
 
     <form action="inscriptionDisplay.php" method="POST">
