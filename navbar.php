cb08bf39a8236e9c203d3cf71aad0c2e9c854e59
Alexandre06Github <124672024+Alexandre06Github@users.noreply.github.com>
2023-03-20 09:05:43 +0100

    navbar

--- navbar.php
+++ navbar.php
@@ -3,9 +3,6 @@
 	<img src="<?php echo ROOT_PATH . "/images/Banniere.jpg" ?>" alt="Description de l'image">
 </header>
 <nav>
-	<div class="logo">
-		<img src="<?php echo ROOT_PATH . "/images/logo.png" ?>" alt="Description du logo">
-	</div>
 	<ul>
 		<li><a class="btn" href="<?php echo ROOT_PATH . "#" ?>">Accueil</a></li>
 		<li><a class="btn" href="<?php echo ROOT_PATH . "voiture/formulaireAnnonce.php" ?>">Déposer une annonce</a></li>
